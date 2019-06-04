32470b57c1d7c49e417d4bba507afdc38cbd685d
olegovnaa <sashatovpeko@mail.ru>
2019-06-04 21:49:37 +0300

    Add breeds from API

--- components/Breeds.js
+++ components/Breeds.js
@@ -1,29 +1,44 @@
-import React, { Component } from 'react';
-import { AppRegistry, Text, View } from 'react-native';
+import React from 'react';
+import { FlatList, ActivityIndicator, Text, View  } from 'react-native';
 
-class Breed extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center'}}>
-        <Text>{this.props.name}</Text>
-      </View>
-    );
+export default class LotsOfBreeds extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state ={ isLoading: true}
   }
-}
+  componentDidMount(){
+    return fetch('https://api.thedogapi.com/v1/breeds')
+      .then((response) => response.json())
+      .then((responseJson) => {
 
-export default class LotsOfBreeds extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center', top: 50}}>
-        <Breed name='Pug' />
-        <Breed name='Beagle' />
-        <Breed name='Bulldog' />
-        <Breed name='Corgi' />
-        <Breed name='Dachshund' />
-        <Breed name='Dalmatian' />
-        <Breed name='Irish Setter' />
+        this.setState({
+          isLoading: false,
+          dataSource: responseJson,
+        }, function(){
 
-      </View>
-    );
+        });
+
+      })
+      .catch((error) =>{
+        console.error(error);
+      });
+  }
+  render(){
+
+    if(this.state.isLoading){
+      return(
+        <View style={{flex: 1, padding: 20}}>
+          <ActivityIndicator/>
+        </View>
+      )
+    }
+     return(
+         <FlatList
+           data={this.state.dataSource}
+           renderItem={({item}) => <Text>{item.name}</Text>}
+           keyExtractor={({id}, index) => id.toString()}
+         />
+     );
   }
 }
