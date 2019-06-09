2d53ea9708078d888f4c1a0c8709ed784feaf82f
olegovnaa <sashatovpeko@mail.ru>
2019-06-09 03:16:50 +0300

    Add Search

--- components/Breeds.js
+++ components/Breeds.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { FlatList, ActivityIndicator, Text, View  } from 'react-native';
-import { ListItem } from 'react-native-elements'
+import { ListItem, SearchBar } from 'react-native-elements'
 
 export default class LotsOfBreeds extends React.Component {
+state = {
+    search: '',
+    };
 
 static navigationOptions = {
   title: 'Breeds',
@@ -12,23 +15,36 @@ static navigationOptions = {
     super(props);
     this.state ={ isLoading: true}
   }
-  componentDidMount(){
-    return fetch('https://api.thedogapi.com/v1/breeds')
-      .then((response) => response.json())
-      .then((responseJson) => {
 
-        this.setState({
-          isLoading: false,
-          dataSource: responseJson,
-        }, function(){
+  getBreeds(url) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => {
 
-        });
+      this.setState({
+        isLoading: false,
+        dataSource: responseJson,
+      }, function(){
 
-      })
-      .catch((error) =>{
-        console.error(error);
       });
+
+    })
+    .catch((error) =>{
+      console.error(error);
+    });
+}
+
+  componentDidMount(){
+    return this.getBreeds('https://api.thedogapi.com/v1/breeds')
   }
+
+  updateSearch = search => {
+    this.setState({ search })
+    if (search.length < 2) return;
+    this.setState({ isLoading: true});
+    return this.getBreeds('https://api.thedogapi.com/v1/breeds/search?q=' + search)
+  };
+
   render(){
 
     if(this.state.isLoading){
@@ -39,11 +55,21 @@ static navigationOptions = {
       )
     }
      return(
+         <View >
+
+         <SearchBar
+           platform="android"
+           placeholder="Search"
+           onChangeText={this.updateSearch}
+           value={this.state.search}
+         />
+
          <FlatList
            data={this.state.dataSource}
            renderItem={({item}) => <ListItem topDivider={true} bottomDivider={true} onPress={() => this.props.navigation.navigate('Breed', {id: item.id, name: item.name})} title={item.name} subtitle={item.breed_group} chevron={true} titleStyle={{ fontWeight: 'bold' }} leftAvatar={{ source: require('../images/logo.jpg') }}/>}
            keyExtractor={({id}, index) => id.toString()}
          />
+         </View>
      );
   }
 }
